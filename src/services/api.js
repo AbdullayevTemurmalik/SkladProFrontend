e1e77f13b9d51096d80ca6a666ba462e2c8550f1
TemurMalik <tmaq77@gmail.com>
2026-05-29 12:54:49 +0500

    SkladProFrontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL:
+    "postgresql://postgres:LZhmLnBOdZCQeSgndLoGpVedSjPObEjQ@zephyr.proxy.rlwy.net:42668/railway",
 });
 
 export default api;
