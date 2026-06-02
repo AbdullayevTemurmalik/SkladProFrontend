58c49d2f7ec82a5b3828e8fc1120613865509c2f
TemurMalik <tmaq77@gmail.com>
2026-06-02 18:35:16 +0500

    SladProFrontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skladprobackend-production.up.railway.app/",
+  baseURL: "https://skladprobackend-production.up.railway.app/api",
 });
 
 export default api;
